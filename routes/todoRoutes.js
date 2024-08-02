b2af84b39c99df42be7dd60803016f6e0f2e990a
Yosif Mohamedain <63748976+Yousif-M-M-H@users.noreply.github.com>
2024-08-02 20:17:29 +0300

    Upade todoRoutes.js

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const todoController = require("../controllers/todoControllers");
 
-// Define routes
 router.post("/", todoController.addTodo);
 router.get("/", todoController.getAllTodos);
 router.get("/:id", todoController.getSingleTodo);
